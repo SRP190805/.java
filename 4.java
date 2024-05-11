c345ea499d80fc2dc84ea86b740244d02dce2bb0
SRP190805 <142969170+SRP190805@users.noreply.github.com>
2024-05-11 07:28:36 -0400

    getting used to it now!

---
+++ 4.java
@@ -0,0 +1,12 @@
+/*Demonstrate the for loop.
+  Call this file "4.java"
+ */
+
+ class ForTest{
+    public static void main(String args[]){
+        int x;
+        
+        for(x=0;x<=10;x++)
+          System.out.println("The value of x is: " + x);
+    }
+ }
