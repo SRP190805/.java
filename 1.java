b355e6eb1c6e2a1d4ae9fae60e27b4a3ab06fa8c
SRP190805 <142969170+SRP190805@users.noreply.github.com>
2024-05-11 06:53:53 -0400

    Have a break! have a java!

---
+++ 1.java
@@ -0,0 +1,10 @@
+/*
+ This is a simple Java Program.
+ Call this file "1.java".
+*/
+
+class Example{
+    public static void main(String args[]){
+        System.out.println("This is a simple java program.");
+    }
+}
