736455444d82bd0be358b8782b18626586cda9d9
SRP190805 <142969170+SRP190805@users.noreply.github.com>
2024-05-11 07:17:01 -0400

    getting the java fever on!

---
+++ 2.java
@@ -0,0 +1,15 @@
+/*
+ here is another short example.
+ Call this file "2.java".
+*/
+
+class Example2{
+    public static void main(String args[]){
+        int num;//this declares a variable called num
+        num = 10;//this assigns the value 10;
+        System.out.println("this num is:" + num);
+        num = num * 2;
+        System.out.print("The value of num*2 is:");
+        System.out.println(num);
+    }
+}
