43125027c7ef328a86646355d04d0292bca5d9fb
SRP190805 <142969170+SRP190805@users.noreply.github.com>
2024-05-16 03:19:00 -0400

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -14,3 +14,25 @@ int result;
        System.out.println("Factorial of 4 is " + f.fact(4));
        System.out.println("Factorial of 5 is " + f.fact(5));
 } }
+
+
+// Another example that uses recursion.
+   class RecTest {
+     int values[];
+     RecTest(int i) {
+       values = new int[i];
+}
+     // display array -- recursively
+     void printArray(int i) {
+       if(i==0) return;
+       else printArray(i-1);
+       System.out.println("[" + (i-1) + "] " + values[i-1]);
+} }
+   class Recursion2 {
+     public static void main(String args[]) {
+       RecTest ob = new RecTest(10);
+       int i;
+       for(i=0; i<10; i++) ob.values[i] = i;
+       ob.printArray(10);
+     }
+}
