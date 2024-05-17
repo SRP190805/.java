281170c3d45cabe8779e64fb4112f997e325fa88
SRP190805 <142969170+SRP190805@users.noreply.github.com>
2024-05-17 07:41:52 -0400

    Update Inheritance.java

--- Inheritance.java
+++ Inheritance.java
@@ -128,3 +128,16 @@ class BoxWeight extends Box {
        System.out.println("Volume of mybox2 is " + vol);
        System.out.println("Weight of mybox2 is " + mybox2.weight);
 } }
+
+
+// Here, Box is extended to include color.
+   class ColorBox extends Box {
+     int color; // color of box
+     ColorBox(double w, double h, double d, int c) {
+       width = w;
+
+ height = h;
+depth = d;
+color = c;
+} }
+
