b54e9c9530f55f078d482d134643c08b004e6a0a
SRP190805 <142969170+SRP190805@users.noreply.github.com>
2024-05-16 23:35:35 -0400

    Update String.java

--- String.java
+++ String.java
@@ -31,3 +31,11 @@
          System.out.println("strOb1 != strOb3");
 } }
 
+
+// Demonstrate String arrays.
+   class StringDemo3 {
+     public static void main(String args[]) {
+       String str[] = { "one", "two", "three" };
+       for(int i=0; i<str.length; i++)
+         System.out.println("str[" + i + "]: " +str[i]);
+} }
