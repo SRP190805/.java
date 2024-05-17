3171c015b9a9396561aedec017981ae79fbaf686
SRP190805 <142969170+SRP190805@users.noreply.github.com>
2024-05-16 22:59:40 -0400

    Update Static.java

--- Static.java
+++ Static.java
@@ -14,3 +14,18 @@
      public static void main(String args[]) {
        meth(42);
 } }
+
+
+class StaticDemo {
+     static int a = 42;
+     static int b = 99;
+     static void callme() {
+       System.out.println("a = " + a);
+     }
+}
+   class StaticByName {
+     public static void main(String args[]) {
+       StaticDemo.callme();
+       System.out.println("b = " + StaticDemo.b);
+     }
+}
